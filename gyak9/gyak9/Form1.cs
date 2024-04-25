1c0a3cae37b21fe1a6e086ff5183ef4c905ae1f4
snwCat <153097647+snwCat@users.noreply.github.com>
2024-04-25 12:34:33 +0200

    Add files via upload

---
+++ gyak9/gyak9/Form1.cs
@@ -0,0 +1,31 @@
+using System.Windows.Forms;
+
+namespace gyak9
+{
+    public partial class Form1 : Form
+    {
+        Models.StudentContext studentContext = new Models.StudentContext();
+        public Form1()
+        {
+            InitializeComponent();
+            studentBindingSource.DataSource = studentContext.Students.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                studentContext.SaveChanges();
+            }
+            catch (Exception kivétel)
+            {
+                MessageBox.Show(kivétel.InnerException.Message);
+            }
+        }
+    }
+}
